b26427900105f180ed8a2294d748a02c54547ff1
Brock Lampman <blampma1@uoregon.edu>
2013-08-07 18:40:08 -0700

    Initial release of 'hello.php'

---
+++ hello.php
@@ -0,0 +1,20 @@
+<?php
+    
+    $html = '<html><head><title>Hello World</title></head><body>';
+    
+    $html .= '<p>Hello Brock</p>';
+    
+    $html .= '</body></html>';
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    echo $html;
+    ?>
