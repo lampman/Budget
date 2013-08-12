3f02382f773b894c1f052789cd60c2c1ccf62a43
Brock Lampman <blampma1@uoregon.edu>
2013-08-11 18:00:17 -0700

    more work done

--- BudgetRefresh.sql
+++ BudgetRefresh.sql
@@ -1,5 +1,4 @@
 USE BUDGET;
---
 DELETE FROM USBANK_CHECK_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/check.csv"
@@ -10,7 +9,6 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 COMMIT;
---
 DELETE FROM USBANK_SAVE_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/save.csv"
@@ -21,7 +19,6 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 COMMIT;
---
 DELETE FROM USBANK_BROCK_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/brock.csv"
@@ -32,7 +29,6 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 COMMIT;
---
 DELETE FROM USBANK_MEGAN_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/megan.csv"
@@ -43,7 +39,6 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 COMMIT;
---
 DELETE FROM CAP_ONE_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/cap_one.csv"
@@ -54,7 +49,10 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 COMMIT;
---
+UPDATE CAP_ONE_INPUT
+SET trans_amount = CASE WHEN TRIM(trans_credit) = '' THEN CONCAT('-',TRIM(trans_debit))
+                        WHEN TRIM(trans_credit) <> '' THEN TRIM(trans_credit) END;
+COMMIT;
 DELETE FROM CHASE_INPUT;
 COMMIT;
 LOAD DATA INFILE "/Applications/MAMP/db/mysql/chase.csv"
@@ -64,4 +62,66 @@ OPTIONALLY ENCLOSED BY '"'
 ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
+COMMIT;
+DELETE FROM PAYEE;
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT CASE WHEN TRIM(SUBSTR(t1.trans_memo,27)) = '' THEN TRIM(t1.trans_name) 
+					 WHEN TRIM(SUBSTR(t1.trans_memo,27)) <> '' THEN TRIM(SUBSTR(t1.trans_memo,27)) END
+                       FROM USBANK_CHECK_INPUT AS t1
+					   WHERE ((TRIM(SUBSTR(t1.trans_memo,27)) <> '' AND 
+                               TRIM(SUBSTR(t1.trans_memo,27)) NOT IN (SELECT t2.payee_memo_name
+																	  FROM PAYEE t2))
+							  OR
+	                          (TRIM(SUBSTR(t1.trans_memo,27)) = '' AND 
+                               TRIM(t1.trans_name)  NOT IN (SELECT t2.payee_memo_name
+															FROM PAYEE t2)));
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT CASE WHEN TRIM(SUBSTR(t1.trans_memo,27)) = '' THEN TRIM(t1.trans_name) 
+					 WHEN TRIM(SUBSTR(t1.trans_memo,27)) <> '' THEN TRIM(SUBSTR(t1.trans_memo,27)) END
+                       FROM USBANK_SAVE_INPUT AS t1
+					   WHERE ((TRIM(SUBSTR(t1.trans_memo,27)) <> '' AND 
+                               TRIM(SUBSTR(t1.trans_memo,27)) NOT IN (SELECT t2.payee_memo_name
+																	  FROM PAYEE t2))
+							  OR
+	                          (TRIM(SUBSTR(t1.trans_memo,27)) = '' AND 
+                               TRIM(t1.trans_name)  NOT IN (SELECT t2.payee_memo_name
+															FROM PAYEE t2)));
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT CASE WHEN TRIM(SUBSTR(t1.trans_memo,27)) = '' THEN TRIM(t1.trans_name) 
+					 WHEN TRIM(SUBSTR(t1.trans_memo,27)) <> '' THEN TRIM(SUBSTR(t1.trans_memo,27)) END
+                       FROM USBANK_BROCK_INPUT AS t1
+					   WHERE ((TRIM(SUBSTR(t1.trans_memo,27)) <> '' AND 
+                               TRIM(SUBSTR(t1.trans_memo,27)) NOT IN (SELECT t2.payee_memo_name
+																	  FROM PAYEE t2))
+							  OR
+	                          (TRIM(SUBSTR(t1.trans_memo,27)) = '' AND 
+                               TRIM(t1.trans_name)  NOT IN (SELECT t2.payee_memo_name
+															FROM PAYEE t2)));
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT CASE WHEN TRIM(SUBSTR(t1.trans_memo,27)) = '' THEN TRIM(t1.trans_name) 
+					 WHEN TRIM(SUBSTR(t1.trans_memo,27)) <> '' THEN TRIM(SUBSTR(t1.trans_memo,27)) END
+                       FROM USBANK_MEGAN_INPUT AS t1
+					   WHERE ((TRIM(SUBSTR(t1.trans_memo,27)) <> '' AND 
+                               TRIM(SUBSTR(t1.trans_memo,27)) NOT IN (SELECT t2.payee_memo_name
+																	  FROM PAYEE t2))
+							  OR
+	                          (TRIM(SUBSTR(t1.trans_memo,27)) = '' AND 
+                               TRIM(t1.trans_name)  NOT IN (SELECT t2.payee_memo_name
+															FROM PAYEE t2)));
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT TRIM(t1.trans_memo) 
+FROM CAP_ONE_INPUT AS t1
+WHERE TRIM(t1.trans_memo) NOT IN (SELECT t2.payee_memo_name
+								  FROM PAYEE t2);
+COMMIT;
+INSERT INTO PAYEE(payee_memo_name)
+SELECT DISTINCT TRIM(t1.trans_memo) 
+FROM CHASE_INPUT AS t1
+WHERE TRIM(t1.trans_memo) NOT IN (SELECT t2.payee_memo_name
+								  FROM PAYEE t2);
 COMMIT;
